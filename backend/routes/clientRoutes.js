7d8ebc928c695d29c1a41f70a118e79eba7db464
Mohit <mohityadaviiitb@gmail.com>
2026-09-17 00:54:03 +0530

    Router for client

---
+++ backend/routes/clientRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import auth from "../middleware/auth.js";
+import { list, create, update, remove } from "../controllers/clientController.js";
+const router = express.Router();
+router.use(auth);
+router.get("/", list);
+router.post("/", create);
+router.put("/:id", update);
+router.delete("/:id", remove);
+export default router;
