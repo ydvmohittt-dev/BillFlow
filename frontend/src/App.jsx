ec897c65cda4ac45adff84b139f4accca56b94b8
Mohit <mohityadaviiitb@gmail.com>
2026-09-18 00:06:40 +0530

    App.jsx file of react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Layout from "./components/Layout";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Clients from "./pages/Clients";
+import ClientForm from "./pages/ClientForm";
+import Invoices from "./pages/Invoices";
+import InvoiceForm from "./pages/InvoiceForm";
+import InvoiceDetails from "./pages/InvoiceDetails";
+import Settings from "./pages/Settings";
+
+const router = createBrowserRouter([
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+  {
+    path: "/",
+    element: (
+      <ProtectedRoute>
+        <Layout />
+      </ProtectedRoute>
+    ),
+    children: [
+      { index: true, element: <Dashboard /> },
+      { path: "clients", element: <Clients /> },
+      { path: "clients/new", element: <ClientForm /> },
+      { path: "clients/:id/edit", element: <ClientForm /> },
+      { path: "invoices", element: <Invoices /> },
+      { path: "invoices/new", element: <InvoiceForm /> },
+      { path: "invoices/:id", element: <InvoiceDetails /> },
+      { path: "settings", element: <Settings /> },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
